26bd50ced4b326c219dc1b4d32b3a5648b0a94ec
Pior Barankiewicz <piotr.barankiewicz@gmail.com>
2022-04-11 10:34:52 +0200

    extension method for VAT computation

---
+++ LevelUpCSharp.Tools/DecimalExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LevelUpCSharp
+{
+	public static class DecimalExtensions
+	{
+		public static decimal Multiplay(this decimal source, int factor)
+		{
+			return source * (1 + (decimal) factor / 100);
+		}
+
+		public static decimal GetVat(this decimal source, int vat)
+		{
+			if (vat < 0 || vat > 23)
+			{
+				throw new ArgumentException("out of range");
+			}
+
+			return source * (1 + (decimal)vat / 100);
+		}
+	}
+}
